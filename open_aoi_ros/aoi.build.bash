60e7ac645f1ceb5bb851752b8487306af47f6fc0
cherniaev egor <chrnyaevek@gmail.com>
2024-04-06 16:15:08 +0200

    Restructure

--- open_aoi_ros/aoi.build.bash
+++ open_aoi_ros/aoi.build.bash
@@ -3,7 +3,7 @@ source /opt/ros/foxy/setup.bash
 # rosdep update --include-eol-distros
 # rosdep install --from-paths src -y --ignore-src
 
-colcon build --packages-select open_aoi_interfaces
-colcon build --packages-select open_aoi_ros --symlink-install
+colcon build --packages-select open_aoi_ros_interfaces
+colcon build --packages-select open_aoi_ros_services --symlink-install
 
 source install/setup.bash
