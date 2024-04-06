3e0964f4acd6ba77faa3b1c3823fa8e1ae659430
cherniaev egor <chrnyaevek@gmail.com>
2024-04-06 16:36:01 +0200

    Make portal node

---
+++ open_aoi_ros/src/open_aoi_portal/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+package_name = "open_aoi_portal"
+setup(
+    name=package_name,
+    version="0.0.1",
+    license="MIT License",
+    packages=[package_name],
+    data_files=[
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Cherniaev Egor",
+    maintainer_email="chrnyaevek@gmail.com",
+    entry_points={
+        "console_scripts": [
+            f"open_aoi_portal = {package_name}.node:main"
+        ],
+    },
+)
