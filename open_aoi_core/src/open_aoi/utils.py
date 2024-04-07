42b17c0717998a71cf3dd8533d9b0ad5a25b7d58
cherniaev egor <chrnyaevek@gmail.com>
2024-04-07 23:34:23 +0200

    Utils

--- open_aoi_core/src/open_aoi/utils.py
+++ open_aoi_core/src/open_aoi/utils.py
@@ -1,9 +1,10 @@
+from typing import List
 import numpy as np
 import cv2 as cv
 from PIL import Image
 
 
-def crop_stat_cv(im: np.ndarray, cv_stat_value: list[int]) -> np.ndarray:
+def crop_stat_cv(im: np.ndarray, cv_stat_value: List[int]) -> np.ndarray:
     # Function parse CV connected component detection statics (values)
     # to cut out component from provided image
     t = cv_stat_value[cv.CC_STAT_TOP]
@@ -15,7 +16,7 @@ def crop_stat_cv(im: np.ndarray, cv_stat_value: list[int]) -> np.ndarray:
     return im[t : t + h, l : l + w]
 
 
-def crop_stat_image(im: Image.Image, cv_stat_value: list[int]) -> Image.Image:
+def crop_stat_image(im: Image.Image, cv_stat_value: List[int]) -> Image.Image:
     # Wrapper for cropping PIL images
     im = np.array(im)
     im = crop_stat_cv(im, cv_stat_value)
--- open_aoi_ros/src/open_aoi_portal/open_aoi_portal/views/view_control_zone_editor.py
+++ open_aoi_ros/src/open_aoi_portal/open_aoi_portal/views/view_control_zone_editor.py
@@ -1,14 +1,12 @@
 # TODO: fix left lower to right upper
 import logging
-from typing import Optional, Tuple, List
-
-import numpy as np
-import cv2 as cv
+from typing import Optional, Tuple
 
 from rclpy.node import Node
 from nicegui import events, ui, app
 from fastapi.responses import RedirectResponse
 
+from open_aoi.utils import crop_stat_image
 from open_aoi.models import TITLE_LIMIT
 from open_aoi.exceptions import AuthException
 from open_aoi.controllers.accessor import AccessorController
@@ -30,24 +28,6 @@ from PIL import Image
 
 logger = logging.getLogger("ui.control_zone_editor")
 
-
-def crop_stat_cv(im: np.ndarray, cv_stat_value: List[int]) -> np.ndarray:
-    # Function parse CV connected component detection statics (values)
-    # to cut out component from provided image
-    t = cv_stat_value[cv.CC_STAT_TOP]
-    l = cv_stat_value[cv.CC_STAT_LEFT]
-
-    w = cv_stat_value[cv.CC_STAT_WIDTH]
-    h = cv_stat_value[cv.CC_STAT_HEIGHT]
-
-    return im[t : t + h, l : l + w]
-
-
-def crop_stat_image(im: Image.Image, cv_stat_value: List[int]) -> Image.Image:
-    # Wrapper for cropping PIL images
-    im = np.array(im)
-    im = crop_stat_cv(im, cv_stat_value)
-    return Image.fromarray(im)
 
 
 class Manager:
