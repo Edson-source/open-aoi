6455cc490c7e4c6296cf0c085ce05d4cdae9c620
cherniaev egor <chrnyaevek@gmail.com>
2024-04-13 09:57:41 +0200

    Restructure

---
+++ open_aoi/src/open_aoi_core/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+package_name = "open_aoi_core"
+setup(
+    name=package_name,
+    version="0.0.1",
+    license="MIT License",
+    packages=[package_name],
+    data_files=[
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Cherniaev Egor",
+    maintainer_email="chrnyaevek@gmail.com"
+)
