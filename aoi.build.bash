82079be35026f6f1c6d25ea36e11f5ff73ed34fc
ChrnyaevEK <36514797+ChrnyaevEK@users.noreply.github.com>
2024-05-17 23:21:56 +0200

    Update aoi.build.bash

--- aoi.build.bash
+++ aoi.build.bash
@@ -1,9 +1,28 @@
+#!/bin/bash
+
+# This script builds selected ROS 2 packages and sets up the workspace
+
+# Source ROS 2 setup script
 source /opt/ros/foxy/setup.bash
 
-colcon build --packages-select open_aoi_interfaces
-colcon build --packages-select open_aoi_gpio --symlink-install
-colcon build --packages-select open_aoi_services --symlink-install
-colcon build --packages-select open_aoi_portal --symlink-install
-colcon build --packages-select open_aoi_core --symlink-install
+# Function to build ROS 2 packages
+build_packages() {
+    if [ "$1" = "open_aoi_interfaces" ]; then
+        colcon build --packages-select $1
+    else
+        colcon build --packages-select $1 --symlink-install
+    fi
+}
 
+# Build selected packages
+echo "Building ROS 2 packages..."
+build_packages "open_aoi_interfaces"
+build_packages "open_aoi_gpio"
+build_packages "open_aoi_services"
+build_packages "open_aoi_portal"
+build_packages "open_aoi_core"
+
+# Source the workspace setup script
 source install/setup.bash
+
+echo "ROS 2 packages built and workspace setup complete."
